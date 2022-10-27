fb25ae3aba3f274111c45fd21d15cda50eb60ca1
Roman <roman.rostock@gmail.com>
2022-10-27 11:53:28 +0200

    added structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,34 @@
 import React from 'react';
 import Home from './pages/Home';
-
-
+import styled from 'styled-components';
+import Navbar from './components/Navbar';
+const Container = styled.div`
+  width:100%;
+`;
+const Wrapper = styled.div`
+  width:100%;
+  background:var(--white);
+  display:flex;
+`;
+const SidebarWrapper = styled.div`
+  flex:1;
+`;
+const ContentWrapper = styled.div`
+  flex:4;
+`;
 const App:React.FC = ()=> {
   return (
-    <div className="App">
-      <Home/>
-    </div>
+    <Container>
+      <Navbar/>
+      <Wrapper>
+        <SidebarWrapper>
+
+        </SidebarWrapper>
+        <ContentWrapper>
+          
+        </ContentWrapper>
+      </Wrapper>
+    </Container>
   );
 }
 
