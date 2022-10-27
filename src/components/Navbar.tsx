2021b10dd58f0b0321161a1628600f70477a4564
Roman <roman.rostock@gmail.com>
2022-10-27 11:53:59 +0200

    designed navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,35 +1,61 @@
 import React from 'react'
-import styled from 'styled-components'
-const Container = styled.div`
+import styled from 'styled-components';
+import {FaShoePrints} from 'react-icons/fa'
+import {IoMdNotificationsOutline, IoMdSettings} from 'react-icons/io'
+const Container = styled.nav`
     width:100%;
-    height:150px;
+    height:80px;
     position:sticky;
     top:0;
     z-index:99;
     display:flex;
     background:var(--coffee);
-`;
-const LogoHolder = styled.div`
-    flex:1;
-    height:100%;
+    display:flex;
     padding:20px;
+`;
+const Logo = styled.div`
+  flex:1;
+  display:flex;
+  align-items:center;
+  color:var(--white);
+  font-size:30px;
 
-    &h1{
-        color:var(--white);
-    }
+  & span{
+    font-family:vivaldi;
+  }
+  & h2{
+    margin-left:20px;
+  }
 `;
-const IconHolder = styled.div`
-    flex:1;
-    height:100%;
-    padding:20px;
+const Greeting = styled.div`
+  flex:1;
+  display:flex;
+  align-items:center;
+  color:var(--white);
+  font-size:30px;
 `;
+const Menue = styled.div`
+  flex:1;
+  display:flex;
+  align-items:center;
+  justify-content: flex-end;
+  color:var(--white);
+  font-size:30px;
+`;
+
 const Navbar:React.FC = () => {
   return (
     <Container>
-      <LogoHolder>
-        <h1>RAR Schumode Dashboard</h1>
-      </LogoHolder>
-      <IconHolder></IconHolder>
+      <Logo>
+        <span>RAR</span>
+        <FaShoePrints/>
+        <h2>Dashboard</h2>
+      </Logo>
+      <Greeting>Hallo Placeholder</Greeting>
+      <Menue>
+        <IoMdNotificationsOutline style={{marginRight:"20px"}}/>
+        <IoMdSettings style={{marginRight:"20px"}}/>
+      </Menue>
     </Container>
   )
 }
