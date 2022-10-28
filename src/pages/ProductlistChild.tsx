4e907fc29f31bd835dcfb13a8c3b92706f9a7995
Roman <roman.rostock@gmail.com>
2022-10-28 09:03:33 +0200

    created Page

---
+++ src/pages/ProductlistChild.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+  width:100%;
+`;
+const ProductlistChild = () => {
+  return (
+    <Container>
+      ProductlistChild
+    </Container>
+  )
+}
+
+export default ProductlistChild
