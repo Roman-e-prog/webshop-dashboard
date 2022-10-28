94908cca643f45fb8633d0519906544debf89719
Roman <roman.rostock@gmail.com>
2022-10-28 09:02:32 +0200

    created Page

---
+++ src/pages/Feedback.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+  width:100%;
+`;
+const Feedback = () => {
+  return (
+    <Container>
+      Feedback
+    </Container>
+  )
+}
+
+export default Feedback
