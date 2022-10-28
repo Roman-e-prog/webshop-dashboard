f866d70e7ffdf653bb713d80ab2b187acfcc89c0
Roman <roman.rostock@gmail.com>
2022-10-28 09:04:25 +0200

    created Page

---
+++ src/pages/Products.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+  width:100%;
+`;
+const Products = () => {
+  return (
+    <Container>
+      Products
+    </Container>
+  )
+}
+
+export default Products
