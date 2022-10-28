a39dfc0699863e6221fda62142cc64227f5824a7
Roman <roman.rostock@gmail.com>
2022-10-28 09:05:13 +0200

    created Page

---
+++ src/pages/Transactions.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+width:100%;
+`;
+const Transactions = () => {
+  return (
+    <Container>
+      Transaktionen
+    </Container>
+  )
+}
+
+export default Transactions
