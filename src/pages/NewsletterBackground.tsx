1ab4f135c52445a8b318585352c7333544cbc331
Roman <roman.rostock@gmail.com>
2022-10-28 09:03:24 +0200

    created Page

---
+++ src/pages/NewsletterBackground.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components';
+const Container = styled.div`
+    width:100%;
+`;
+const NewsletterBackground = () => {
+  return (
+    <Container>
+      NewsletterBackground
+    </Container>
+  )
+}
+
+export default NewsletterBackground
