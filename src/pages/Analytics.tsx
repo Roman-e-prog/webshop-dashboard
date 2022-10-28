04956754037ac9686747fef7f02f403dec7fe5bb
Roman <roman.rostock@gmail.com>
2022-10-28 09:01:40 +0200

    created Page Analytics

---
+++ src/pages/Analytics.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+  width:100%;
+`;
+const Analytics = () => {
+  return (
+    <Container>
+      Analytics
+    </Container>
+  )
+}
+
+export default Analytics
