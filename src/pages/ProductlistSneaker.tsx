54edcce6e6b260ed90ab28c24e694f1302aa84a0
Roman <roman.rostock@gmail.com>
2022-10-28 09:03:45 +0200

    created Page

---
+++ src/pages/ProductlistSneaker.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+const Container = styled.div`
+  width:100%;
+`;
+const ProductlistSneaker = () => {
+  return (
+    <Container>
+      ProductlistSneaker
+    </Container>
+  )
+}
+
+export default ProductlistSneaker
