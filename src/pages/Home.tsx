f3cabc8432379b81cbc873bfbb5a3a1e6add7c81
Roman <roman.rostock@gmail.com>
2022-10-31 09:01:34 +0100

    designed charts

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import Chart from '../components/Chart';
 import Revenue from '../components/Revenue';
+import Transactions from '../components/Transactions';
+import UserInfo from '../components/UserInfo';
 const Container = styled.div`
   width:100%;
+`;
+const ZusatzInfos = styled.div`
+  display:flex;
 `;
 const Home:React.FC = () => {
   return (
     <Container>
       <Revenue/>
+      <Chart/>
+      <ZusatzInfos>
+        <UserInfo/>
+        <Transactions/>
+      </ZusatzInfos>
     </Container>
   )
 }
