b19d2c1a649c9ec07dfe8779cbbc41f7ff9af574
Roman <roman.rostock@gmail.com>
2023-03-11 15:29:26 +0100

    added validation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import styled from 'styled-components'
-import {useState, useEffect} from 'react';
+import {useState, useEffect, useCallback} from 'react';
 import { RootState } from '../app/store';
-import { login, reset} from '../features/authSlice'
+import { login, reset} from '../features/authSlice';
 import { useAppDispatch } from '../app/hooks';
 import { useSelector } from 'react-redux';
 import {toast} from 'react-toastify'
 import { useNavigate } from 'react-router-dom';
+import { LoginSchema } from '../validations/LoginValidation';
+import update from 'immutability-helper';
 const Container = styled.div`
     width:100%;
     height:100vh;
@@ -55,7 +57,14 @@ const Login = () => {
         password:"",
     });
     const {username, email, password} = formdata;
-
+    //validation
+    const [formerror, setFormerror] = useState({
+        username:false,
+        email:false,
+        password:false,
+        error:[],
+    })
+console.log(formerror);
 useEffect(()=>{
     if(isError){
         toast.error(message);
@@ -74,29 +83,66 @@ const handleChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
         [e.target.name]: e.target.value,
     }))
 }
-const onSubmit = (e:React.FormEvent)=>{
-    e.preventDefault();
-    const loginData = {
-        username,
-        email,
-        password,
-    }
-    dispatch(login(loginData))
-}
+const onSubmit = useCallback(
+    async (e:React.FormEvent) => {
+      e.preventDefault()
+      const loginData = {
+                username,
+                email,
+                password,
+            }
+      // Check the schema if form is valid:
+      const isFormValid = await LoginSchema.isValid(loginData, {
+        abortEarly: false, // Prevent aborting validation after first error
+      })
+
+      if (isFormValid) {
+        dispatch(login(loginData))
+      } else {
+        // If form is not valid, check which fields are incorrect:
+        LoginSchema.validate(loginData, { abortEarly: false }).catch((err) => {
+          const errors = err.inner.reduce((acc:any, error:any) => {
+            return {
+              ...acc,
+              [error.path]: error.errors,
+            }
+          }, {})
+            console.log(errors);
+          // Update form errors state:
+          setFormerror((prevErrors:any) =>
+            update(prevErrors, {
+              $set: errors,
+            })
+          )
+        })
+      }
+    },
+    [dispatch, email, password, username]
+  )
+
   return (
     <Container>
       <LoginForm onSubmit={onSubmit}>
         <FormGroup>
             <label htmlFor='username'>Benutzername</label>
             <input type="text" name="username" id="username" required value={username} onChange={handleChange}/>
+            <div>
+                {formerror.username && <span>{formerror.username}</span>}
+            </div>
         </FormGroup>
         <FormGroup>
             <label htmlFor='email'>E-mail</label>
             <input type="email" name="email" id="email" required  value={email} onChange={handleChange}/>
+            <div>
+            {formerror.email && <span>{formerror.email}</span>}
+            </div>
         </FormGroup>
         <FormGroup>
             <label htmlFor='password'>Password</label>
             <input type="password" name="password" id="password" required  value={password} onChange={handleChange}/>
+            <div>
+            {formerror.password && <span>{formerror.password}</span>}
+            </div>
         </FormGroup>
         <SubmitButton onClick={onSubmit}>Login</SubmitButton>
       </LoginForm>
