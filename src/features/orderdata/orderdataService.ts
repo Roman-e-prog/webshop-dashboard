17db6008c4b2e18fde9e69aa7fcafa5be6faca48
Roman <roman.rostock@gmail.com>
2023-03-11 15:27:05 +0100

    created orderdataService

---
+++ src/features/orderdata/orderdataService.ts
@@ -0,0 +1,49 @@
+import axios from "axios";
+
+const API_URL = 'http://localhost:5001/api/orderdata/'
+
+const deleteOrderdata = async (orderId:string, token:string)=>{
+    const config = {
+        headers:{
+            token: `Bearer ${token}`
+        }
+    }
+    const response = await axios.delete(API_URL + orderId, config)
+    return response.data;
+}
+const getOrderdata = async (orderId:string, token:string)=>{
+    const findUrl = `find/${orderId}`
+    const config = {
+        headers:{
+            token: `Bearer ${token}`
+        }
+    }
+    const response = await axios.get(API_URL + findUrl, config)
+    console.log(response.data);
+    return response.data;
+}
+const getAllOrderdata = async (token:string)=>{
+    const config = {
+        headers:{
+            token: `Bearer ${token}`
+        }
+    }
+    const response = await axios.get(API_URL + 'find', config)
+    return response.data;
+}
+const getIncome = async (token:string)=>{
+    const config = {
+        headers:{
+            token: `Bearer ${token}`
+        }
+    }
+    const response = await axios.get(API_URL + 'income', config)
+    return response.data;
+}
+const orderdataService = {
+    deleteOrderdata,
+    getOrderdata,
+    getAllOrderdata,
+    getIncome,
+}
+export default orderdataService;
