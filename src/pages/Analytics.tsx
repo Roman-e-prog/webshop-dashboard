264ed09710e039efa176d882311e9a73a01f52b1
Roman <roman.rostock@gmail.com>
2023-03-11 15:28:46 +0100

    analytics

--- src/pages/Analytics.tsx
+++ src/pages/Analytics.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
+
 const Container = styled.div`
   width:100%;
 `;
 const Analytics = () => {
+
   return (
     <Container>
       Analytics
