4b0f3c173c7b4753b5af517ba0afcb7c7ece3aaa
Roman <roman.rostock@gmail.com>
2023-03-11 15:29:12 +0100

    changes

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,8 @@ import Chart from '../components/Chart';
 import Revenue from '../components/Revenue';
 import Transactions from '../components/Transactions';
 import UserInfo from '../components/UserInfo';
-import { useEffect } from 'react';
+import { useEffect, useState, useMemo} from 'react';
+import { getUserStats } from '../features/user/userSlice';
 const Container = styled.div`
   width:100%;
 `;
@@ -24,10 +25,46 @@ const Home:React.FC = () => {
       navigate('/');
     }
   },[dispatch, user?.isAdmin, navigate])
+//chart
+const activeUser = useAppSelector((state)=>state.user.userStats);
+const [stats, setStats] = useState<any>([]);
+useEffect(()=>{
+  dispatch(getUserStats())
+},[dispatch]);
+const Month = useMemo(
+  () => [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Agu",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ],
+  []
+);
+
+useEffect(()=>{
+      activeUser.map((item)=>setStats((prev:any)=>[...prev, {name:Month[parseInt(item._id!)-1], "Active User": item.total}]))
+      //eslint-disable-next-line
+},[]);
+
   return (
     <Container>
-      <Revenue/>
-      <Chart/>
+      <Revenue
+      month={Month}
+      />
+      <Chart 
+      userdata ={stats}
+      title = "User Analyse"
+      grid
+      dataKey={"Active User"}
+      />
       <ZusatzInfos>
         <UserInfo/>
         <Transactions/>
