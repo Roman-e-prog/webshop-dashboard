fa960c2d41b4a639adb37803c358a9123c9ee72d
Roman <roman.rostock@gmail.com>
2023-03-11 15:25:23 +0100

    created revenue

--- src/components/Revenue.tsx
+++ src/components/Revenue.tsx
@@ -1,56 +1,78 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import styled from 'styled-components'
+import { useAppDispatch, useAppSelector } from '../app/hooks';
+import { getIncome } from '../features/orderdata/orderdataSlice';
 const Container = styled.div`
+    width:100%;
+`;
+const Einnahmen = styled.div`
     width:100%;
     display:flex;
 `;
-const Einnahmen = styled.div`
+const TagesEinnahmen = styled.div`
     flex:1;
     display:flex;
     flex-direction:column;
     align-items:center;
 `;
-const Sales = styled.div`
+const MonatsEinnahmen = styled.div`
     flex:1;
     display:flex;
     flex-direction:column;
     align-items:center;
 `;
-const Kosten = styled.div`
+const JahresEinnahmen = styled.div`
     flex:1;
     display:flex;
     flex-direction:column;
     align-items:center;
 `;
 const Title = styled.h3`
-    font-size:26px;
+    font-size:20px;
 `;
 const Amount = styled.span`
     font-size:30px;
     margin:20px 0;
 `;
-const Hint = styled.p`
-    font-size:20px;
-    color:var(--darkGray);
-`;
-const Revenue = () => {
+
+const Revenue = (props:{month:string[]}) => {
+  const dispatch = useAppDispatch();
+  const income = useAppSelector((state)=>state.orderdata.income);
+
+  const date = new Date();
+  const lastDay = new Date(date.setDate(date.getDate()-1));
+  const lastMonth = new Date().getMonth();
+  const year = new Date().getFullYear();
+
+  useEffect(()=>{
+    dispatch(getIncome())
+  },[dispatch])
+
+  const dayIncome = income.filter((item:any)=>item._id.day === lastDay);
+  const monthIncome = income.filter((item:any)=>item._id.month === lastMonth);
+  const yearIncome = income.filter((item:any)=>item._id.year === year);
+  let totalMonth = monthIncome.reduce((accum,item) => accum + item.total!, 0);
+  let totalYear = yearIncome.reduce((accum,item) => accum + item.total!, 0);
+
   return (
     <Container>
-      <Einnahmen>
-        <Title>Einnahmen</Title>
-        <Amount>Summe</Amount>
-        <Hint>Verglichen mit letztem Monat</Hint>
+    <Einnahmen>
+        <TagesEinnahmen>
+          <Title>{`Tageseinnahmen vom ${lastDay.toLocaleDateString("de-De",
+          {weekday:'short', 
+          day:'numeric', 
+          month:'numeric'})}`}</Title>
+             <Amount>{dayIncome.length ? <span>{dayIncome[0].total} €</span>: <span>0 €</span>}</Amount>
+        </TagesEinnahmen>
+        <MonatsEinnahmen>
+          <Title>{`Einnahmen im ${props.month[lastMonth-1]}`}</Title>
+          <Amount>{totalMonth} €</Amount>
+        </MonatsEinnahmen>
+        <JahresEinnahmen>
+          <Title>{`Einnahmen ${year}`}</Title>
+          <Amount>{totalYear} €</Amount>
+        </JahresEinnahmen>
       </Einnahmen>
-      <Sales>
-        <Title>Sales</Title>
-        <Amount>Summe</Amount>
-        <Hint>Verglichen mit letztem Monat</Hint>
-      </Sales>
-      <Kosten>
-        <Title>Kosten</Title>
-        <Amount>Summe</Amount>
-        <Hint>Verglichen mit letztem Monat</Hint>
-      </Kosten>
     </Container>
   )
 }
