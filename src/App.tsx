68538a9486458fc1d29f7f5d7fdcfba8c68bf107
Roman <roman.rostock@gmail.com>
2023-03-11 15:22:45 +0100

    protected routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {Fragment} from 'react';
 import Home from './pages/Home';
 import styled from 'styled-components';
 import Navbar from './components/Navbar';
@@ -25,13 +24,15 @@ import SneakerImage from './pages/SneakerImage';
 import SliderImages from './pages/SliderImages';
 import DescriptionItems from './pages/DescriptionItems';
 import NewsletterBackground from './pages/NewsletterBackground';
-import Login from './pages/Login';
 import { useAppSelector } from './app/hooks';
 import UserDisplay from './pages/UserDisplay';
 import ProductEdit from './pages/ProductEdit';
 import DescriptionItemsEdit from './pages/DescriptionItemsEdit';
 import SliderItemsEdit from './pages/SliderItemsEdit';
 import NotFound from './pages/NotFound';
+import SalesBill from './pages/SalesBill';
+import ProtectedPage from './utils/ProtectedPage';
+import Login from './pages/Login';
 const Container = styled.div`
   width:100%;
 `;
@@ -49,54 +50,50 @@ const ContentWrapper = styled.div`
 const App:React.FC = ()=> {
     const admin = useAppSelector((state)=>state.auth.user?.isAdmin);
   return (
-   
     <Container>
-    <Router>
-      <Routes>
-      <Route path="/" element={<Login/>}/>
-      </Routes>
-      {admin && 
-    <>
-      <Navbar/>
+      {admin ? <Navbar/> : null}
       <Wrapper>
-        <Sidebar/>
+       {admin ? <Sidebar/> : null} 
         <ContentWrapper>
-          <Routes>
-            <Route path="*" element={<NotFound />} />
-              <Route path="/home" element={<Home/>}/>
-              <Route path="/sales" element={<Sales/>}/>
-              <Route path="/analytics" element={<Analytics/>}/>
-              <Route path="/transactions" element={<Transactions/>}/>
-              <Route path="/reports" element={<Reports/>}/>
-              <Route path="/mail" element={<Mail/>}/>
-              <Route path="/feedback" element={<Feedback/>}/>
-              <Route path="/messages" element={<Messages/>}/>
-              <Route path="/manage" element={<Manage/>}/>
-              <Route path="/user" element={<User/>}/>
-              <Route path="/products" element={<Products/>}/>
-              <Route path="/productlistChild" element={<ProductlistChild/>}/>
-              <Route path="/productlistWomen" element={<ProductlistWomen/>}/>
-              <Route path="/productlistMen" element={<ProductlisttMen/>}/>
-              <Route path="/productlistSportschuhe" element={<ProductlistSportschuhe/>}/>
-              <Route path="/productlistSneaker" element={<ProductlistSneaker/>}/>
-              <Route path="/cardImages" element={<CardImages/>}/>
-              <Route path="/sneakerImage" element={<SneakerImage/>}/>
-              <Route path="/sliderImage" element={<SliderImages/>}/>
-              <Route path="/newsletterBackground" element={<NewsletterBackground/>}/>
-              <Route path="/descriptionItems" element={<DescriptionItems/>}/>
-              {/* Edits */}
-              <Route path="/showUser/:id" element={<UserDisplay/>}/>
-              <Route path="/showProduct/:id" element={<ProductEdit/>}/>
-              <Route path="/showDescriptionItem/:id" element={<DescriptionItemsEdit/>}/>
-              <Route path="/showSliderItem/:id" element={<SliderItemsEdit/>}/>
+              <Routes>
+                <Route path="/" element={<Login/>}/>
+                <Route element={<ProtectedPage/>}>
+                      <Route path="*" element={<NotFound />} />
+                      <Route path="/home" element={<Home/>}/>
+                      <Route path="/sales" element={<Sales/>}/>
+                      <Route path="/analytics" element={<Analytics/>}/>
+                      <Route path="/transactions" element={<Transactions/>}/>
+                      <Route path="/reports" element={<Reports/>}/>
+                      <Route path="/mail" element={<Mail/>}/>
+                      <Route path="/feedback" element={<Feedback/>}/>
+                      <Route path="/messages" element={<Messages/>}/>
+                      <Route path="/manage" element={<Manage/>}/>
+                      <Route path="/user" element={<User/>}/>
+                      <Route path="/products" element={<Products/>}/>
+                      <Route path="/productlistChild" element={<ProductlistChild/>}/>
+                      <Route path="/productlistWomen" element={<ProductlistWomen/>}/>
+                      <Route path="/productlistMen" element={<ProductlisttMen/>}/>
+                      <Route path="/productlistSportschuhe" element={<ProductlistSportschuhe/>}/>
+                      <Route path="/productlistSneaker" element={<ProductlistSneaker/>}/>
+                      <Route path="/cardImages" element={<CardImages/>}/>
+                      <Route path="/sneakerImage" element={<SneakerImage/>}/>
+                      <Route path="/sliderImage" element={<SliderImages/>}/>
+                      <Route path="/newsletterBackground" element={<NewsletterBackground/>}/>
+                      <Route path="/descriptionItems" element={<DescriptionItems/>}/>
+                      {/* Edits */}
+                      <Route path="/showUser/:id" element={<UserDisplay/>}/>
+                      <Route path="/showProduct/:id" element={<ProductEdit/>}/>
+                      <Route path="/showDescriptionItem/:id" element={<DescriptionItemsEdit/>}/>
+                      <Route path="/showSliderItem/:id" element={<SliderItemsEdit/>}/>
+                      {/* Bill */}
+                      <Route path="/bill/:id" element={<SalesBill/>}/>
+                </Route>
               </Routes>
           </ContentWrapper>
-        </Wrapper>
-        </>
-        }
-      </Router>
+        </Wrapper> 
     </Container>
   );
 }
+
 
 export default App;
